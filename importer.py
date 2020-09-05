8de9d3b5daae51b4e48401ed870a4963ab726506
alam1008 <angela.lam@utah.edu>
2020-09-05 04:05:11 -0600

    importer.py file

---
+++ importer.py
@@ -0,0 +1,44 @@
+Last login: Sat Sep  5 03:50:52 on ttys002
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Angelas-MBP:~ angela$ python
+Python 3.8.3 (default, Jul  2 2020, 11:26:31) 
+[Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import sys
+>>> sys.path
+['', '/opt/anaconda3/lib/python38.zip', '/opt/anaconda3/lib/python3.8', '/opt/anaconda3/lib/python3.8/lib-dynload', '/opt/anaconda3/lib/python3.8/site-packages', '/opt/anaconda3/lib/python3.8/site-packages/aeosa']
+>>> sys.path.append(r'/Users/angela/Documents/BMI/NURS 6806 Computer Science Fundamentals/myProjects/exerPythModPackTutorial')
+>>> sys.path
+['', '/opt/anaconda3/lib/python38.zip', '/opt/anaconda3/lib/python3.8', '/opt/anaconda3/lib/python3.8/lib-dynload', '/opt/anaconda3/lib/python3.8/site-packages', '/opt/anaconda3/lib/python3.8/site-packages/aeosa', '/Users/angela/Documents/BMI/NURS 6806 Computer Science Fundamentals/myProjects/exerPythModPackTutorial']
+>>> import mod
+>>> import fact
+>>> print(mod)
+<module 'mod' from '/Users/angela/Documents/BMI/NURS 6806 Computer Science Fundamentals/myProjects/exerPythModPackTutorial/mod.py'>
+>>> print(fact)
+<module 'fact' from '/Users/angela/Documents/BMI/NURS 6806 Computer Science Fundamentals/myProjects/exerPythModPackTutorial/fact.py'>
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
