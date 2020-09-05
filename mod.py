d399fbd185b98d4c0dcfbfcf139d37e81a094686
alam1008 <angela.lam@utah.edu>
2020-09-05 04:03:23 -0600

    mod.py file

---
+++ mod.py
@@ -0,0 +1,16 @@
+s = "If Comrade Napoleon says it, it must be right."
+a = [100, 200, 300]
+
+def foo (arg):
+    print(f'arg = {arg}')
+
+    class Foo:
+        pass
+
+if ('_name_'=='_main_'):
+    print('Executing as a standalone script') 
+    print(s)
+    print(a)
+    foo('quux')
+    x = Foo()
+    print(x)
